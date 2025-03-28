2b34f4cfea688ef6b7a43c61c02d82d2a449c922
root <root@ctr-ubbsmc16.amd.com>
2025-03-28 03:50:28 -0400

    clean

---
+++ lightinfer/wrapper/python/pytorch/gemm/linear.py
@@ -0,0 +1,13 @@
+"""
+Linear API
+"""
+
+from typing import Callable, Dict, Optional, Tuple, Union
+
+class Linear:
+    def __init__(self, in_features:int, ):
+        pass
+    
+
+    def forward():
+        
