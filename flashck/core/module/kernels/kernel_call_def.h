f9b3dd05f6eaefbda20d1d211297eaebf2de735b
Yanxing-Shi <yanxishi@amd.com>
2025-07-15 10:57:37 +0000

    norm success

--- flashck/core/module/kernels/kernel_call_def.h
+++ flashck/core/module/kernels/kernel_call_def.h
@@ -11,8 +11,8 @@ namespace flashck {
         if (!ProfilingEngine::GetInstance()->GetKernelLibrary()->has_symbol(name_str)) {                               \
             FC_THROW(Unavailable("Kernel symbol not found {}", name_str));                                             \
         }                                                                                                              \
-        kernel_func = *(ProfilingEngine::GetInstance()->GetKernelLibrary()->get_function<decltype(kernel_func)>(       \
-            kernel_func_name));                                                                                        \
+        kernel_func = ProfilingEngine::GetInstance()->GetKernelLibrary()->get2_function<decltype(kernel_func)>(        \
+            kernel_func_name);                                                                                         \
     } while (0)
 
 }  // namespace flashck
